7308ddec6d52e31b28fac386a48592ca1e60f03d
peter laliberte <peter.laliberte@burgzergarcade.net>
2017-04-29 11:10:34 -0400

    Initial push to GitHub.

---
+++ Blink/Assets/Blinky.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+//using UnityEngine.UI;
+
+//[RequireComponent(typeof(Text))]
+public class Blinky : MonoBehaviour {
+    [SerializeField] float blinkTime = 1f;
+
+    [SerializeField]MonoBehaviour comp;
+//    Text text;
+
+
+
+//    void Awake()
+//    {
+//        text = GetComponent<Text>();
+//    }
+
+
+
+    void Start ()
+    {
+        InvokeRepeating("Blink", blinkTime, blinkTime);
+	}
+
+
+
+    void Blink()
+    {
+        comp.enabled = !comp.enabled;
+//        text.enabled = !text.enabled;
+    }
+}
